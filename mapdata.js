7de5696932617215ec9ccdbb7b6235c570dcec8d
zeinab315 <39467873+zeinab315@users.noreply.github.com>
2018-07-24 17:59:06 +0300

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,199 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#ffffff",
+    background_transparent: "no",
+    border_color: "#000000",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#rrrrrr",
+    state_hover_color: "#99b5cd",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "yes",
+    hide_eastern_labels: "yes",
+   
+    //Zoom settings
+    zoom: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "yes",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    "0": {},
+    SYR134: {
+      name: "Lattakia",
+      color: "#123456",
+      hover_color: "#3B729F",
+      description: "اللاذقية",
+      url: "lattakia.html",
+      image_url: "lattakia"
+    },
+    SYR135: {
+      name: "Tartus",
+      color: "#234567",
+      hover_color: "#3B729F",
+      description: "طرطوس",
+      url: "tartuse.html",
+      image_url: "test image"
+    },
+    SYR136: {
+      name: "Ar Raqqah",
+      color: "#345678",
+      hover_color: "#3B729F",
+      description: "الرقة",
+      url: "al-raqqah.html",
+      image_url: "test image"
+    },
+    SYR137: {
+      name: "Aleppo",
+      color: "#a6924a",
+      hover_color: "#3B729F",
+      description: "حلب",
+      url: "aleppo.html",
+      image_url: "test image"
+    },
+    SYR138: {
+      name: "Hamah",
+      color: "#cd96b0",
+      hover_color: "#3B729F",
+      description: "حماه",
+      url: "hamah.html",
+      image_url: "test image"
+    },
+    SYR139: {
+      name: "Homs (Hims)",
+      color: "#e2b8cc",
+      hover_color: "#3B729F",
+      description: "حمص",
+      url: "homs.html",
+      image_url: "test image"
+    },
+    SYR140: {
+      name: "Idlib",
+      color: "#a6d8b4",
+      hover_color: "#3B729F",
+      description: "إدلب",
+      url: "idlib.html",
+      image_url: "test image"
+    },
+    SYR141: {
+      name: "Hasaka (Al Haksa)",
+      color: "#9ce5b0",
+      hover_color: "#3B729F",
+      description: "الحسكة",
+      url: "hasaka.html",
+      image_url: "test image"
+    },
+    SYR142: {
+      name: "Dayr Az Zawr",
+      color: "#d0de91",
+      hover_color: "#3B729F",
+      description: "دير الزرو",
+      url: "dayr-al-zawr.html",
+      image_url: "test image"
+    },
+    SYR143: {
+      name: "As Suwayda'",
+      color: "#e4d293",
+      hover_color: "#3B729F",
+      description: "السويداء",
+      url: "al-suwayda.html",
+      image_url: "test image"
+    },
+    SYR144: {
+      name: "Rif Dimashq",
+      color: "#d8a298",
+      hover_color: "#3B729F",
+      description: "ريف دمشق",
+      url: "test",
+      image_url: "test image"
+    },
+    SYR145: {
+      name: "Quneitra",
+      color: "#8de7e7",
+      hover_color: "#3B729F",
+      description: "القنيطرة",
+      url: "rif-dimashq.html",
+      image_url: "test image"
+    },
+    SYR146: {
+      name: "Dar`a",
+      color: "#d8929b",
+      hover_color: "#3B729F",
+      description: "درعا",
+      url: "daraa.html",
+      image_url: "rif-dimashq.html"
+    },
+    SYR4843: {
+      name: "Damascus",
+      color: "#d89297",
+      hover_color: "#3B729F",
+      description: "دمشق",
+      url: "damascus.html",
+      image_url: "test image"
+    },
+  },
+  locations: {
+    "0": {
+      lat: "33.512529",
+      lng: "36.278016",
+      name: "Damascus",
+	   url: "damascus.html"
+    }
+  },
+  labels: {},
+  regions: {}
+};
